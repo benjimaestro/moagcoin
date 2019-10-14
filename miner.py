d937602dfc6a1401a93798a074ce48fc3de34f39
Ben <benjamin.gjones@yahoo.co.uk>
2019-10-15 00:14:49 +0100

    Sicko mode added
    Multiple issues fixed, such as mining not stopping properly, mining not working on Linux
    Added SICKO MODE, allowing miner to use multiple cores (process count depends on cores available)
    Added unix timestamps to mined blocks

--- miner.py
+++ miner.py
@@ -15,46 +15,7 @@ import hashlib
 import ecdsa
 import base64
 from ast import literal_eval
-try:
-    import winsound#probably doesnt work on linux, fuck linux
-except:
-    pass
-
-root = Tk()
-root.configure(background='black')
-root.title("MoagCoin")
-try:
-    root.iconbitmap('res\\icon.ico')
-except:
-    try:
-        root.iconbitmap('res/icon.ico')#fuckin linux users, man. will this work? idk
-    except:
-        pass
-frames = [PhotoImage(file='res\\MoagCoin.gif',format = 'gif -index %i' %(i)) for i in range(79)]
-
-
-print("""
- ███▄ ▄███▓ ▒█████   ▄▄▄        ▄████  ▄████▄   ▒█████   ██▓ ███▄    █ 
-▓██▒▀█▀ ██▒▒██▒  ██▒▒████▄     ██▒ ▀█▒▒██▀ ▀█  ▒██▒  ██▒▓██▒ ██ ▀█   █ 
-▓██    ▓██░▒██░  ██▒▒██  ▀█▄  ▒██░▄▄▄░▒▓█    ▄ ▒██░  ██▒▒██▒▓██  ▀█ ██▒
-▒██    ▒██ ▒██   ██░░██▄▄▄▄██ ░▓█  ██▓▒▓▓▄ ▄██▒▒██   ██░░██░▓██▒  ▐▌██▒
-▒██▒   ░██▒░ ████▓▒░ ▓█   ▓██▒░▒▓███▀▒▒ ▓███▀ ░░ ████▓▒░░██░▒██░   ▓██░
-░ ▒░   ░  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ░▒   ▒ ░ ░▒ ▒  ░░ ▒░▒░▒░ ░▓  ░ ▒░   ▒ ▒ 
-░  ░      ░  ░ ▒ ▒░   ▒   ▒▒ ░  ░   ░   ░  ▒     ░ ▒ ▒░  ▒ ░░ ░░   ░ ▒░
-░      ░   ░ ░ ░ ▒    ░   ▒   ░ ░   ░ ░        ░ ░ ░ ▒   ▒ ░   ░   ░ ░ 
-       ░       ░ ░        ░  ░      ░ ░ ░          ░ ░   ░           ░ 
-                                      ░                                
-                    --===============================--
-                          MOAGCOIN MINER & WALLET
-                    --===============================--
-""")#highly important performance enhancing ascii art
-
-music = ["money","keygen","bangarang","midi","sprites","minz","tripoloski","tiananmen"]#'minz' is the audio from a lineage rom review btw
-
-try:
-    winsound.PlaySound("res\\"+music[random.randint(0,len(music)-1)]+".wav", winsound.SND_ASYNC)#in error catch thingy because fuck linux
-except:
-    pass#in case music is deleted or linux is used
+import multiprocessing as mp
 
 def generate_wallet():
     sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
@@ -194,73 +155,145 @@ def wallet():#Wallet window, called when open wallet is pressed, fairly safe to 
             listbox.insert("end", "   BLOCK STATUS: "+str(block["validated"]))
             listbox.insert("end", "-----------------")
 
-class miner():#Handles the proof of work mining of blocks - do not edit this, or mined blocks will not verify properly and you'll have wasted a bunch of CPU cycles
+##        if mp.cpu_count() == 1:                         #This block determintes how many processes should be spawned
+##            self.cores = 1                              #when mining is done with multiprocessing
+##        if mp.cpu_count() > 1 and mp.cpu_count() <= 6:  #it's OK to modify this but keep in mind that your PC will
+##            self.cores = mp.cpu_count() - 1             #slow considerably if you don't keep some cores open.
+##        if mp.cpu_count() > 6:                          #
+##            self.cores = mp.cpu_count() - 2             #
+
+class varSave():
     def __init__(self):
-        self.quit = True#Tells mining thread when to quit, is toggled by button
-        self.size = 80#Cannot be changed, or your confirmed blocks will be rejected
-    def miner(self):
-        while self.quit == False:
-            print("Starting miner...")
-            found = False
-            time.sleep(1)
-            publicAddress = read_wallet()['public_key']
-            blockRequest = {"header":"request-block","address":publicAddress}
-            block = requests.get('http://35.209.72.253:443/unclaimedBlock')
-            block = ast.literal_eval(block.text)
-            if block != False:
-                print("Got block "+str(block["hash"]))
-                challenge = block["hash"] + str(self.size)
-                start = time.time()
-                while self.quit == False and found == False:
-                    answer = "".join(random.choice(string.ascii_lowercase+
-                                       string.ascii_lowercase+
-                                       string.digits) for x in range(self.size))
-                    attempt = challenge+answer
-                    solution = hashlib.sha256(attempt.encode()).hexdigest()
-                    if solution.startswith('000000'):#DONT change this either, will be rejected by server
+        self.quit = True
+
+def miner(vs,quitEvent):
+    procs = []#Stores processes, best not to mess with
+    publicAddress = read_wallet()['public_key']
+
+    if mp.cpu_count() == 1:                         #This block determintes how many processes should be spawned
+        cores = 1                                   #when mining is done with multiprocessing
+    if mp.cpu_count() > 1 and mp.cpu_count() <= 6:  #it's OK to modify this but keep in mind that your PC will
+       cores = mp.cpu_count() - 1                   #slow considerably if you don't keep some cores open.
+    if mp.cpu_count() > 6:                          #
+       cores = mp.cpu_count() - 2                   #
+    
+    print("Starting miner with "+str(cores)+" cores...")
+    print("SICKO MODE ENGAGED, using "+str(cores)+" cores!")
+    while quitEvent.is_set() == False:
+        found = False
+        event = mp.Event()
+        time.sleep(1)
+        blockRequest = {"header":"request-block","address":publicAddress}
+        block = requests.get('http://35.209.72.253:443/unclaimedBlock')
+        block = ast.literal_eval(block.text)
+        print("\nGot block "+str(block["hash"]))
+        print("Mining...")
+        if block != False:
+            for i in range(cores):#Creates new mining process depending on how many CPU cores you have
+                p = mp.Process(target=proofOfWork,args=(event,block,publicAddress,))
+                p.start()#Spawn new process to mine block
+                procs.append(p)#Adds processes to a list so they can be accessed later
+            while found == False and vs.quit == False:
+                if event.is_set():
+                    for process in procs:#Loops through process list to kill processes that did not complete the block in tie
+                        process.terminate()
                         found = True
-                        print("BLOCK MINED! Time taken:", time.time()-start)
-                        print(solution)
-                        start = time.time()
-                        block["attempt"] = attempt
-                        block["miner"] = publicAddress
-                        time.sleep(1)
-                        res = requests.post("http://35.209.72.253:443/confirmBlock", data=block)
-            else:
-                print("Mining error, server did not return block")
-Miner = miner()
-def minerButton(minerClass):#run when the mining button is pressed, best not to edit this
-    if minerClass.quit == True:
-        minerClass.quit = False
+                    procs.clear()
+                time.sleep(0.5)
+            found = False
+        else:
+            print("Mining error, server did not return block")
+    if quitEvent.is_set():#Kill processes if stop button pressed
+        for process in procs:
+            process.terminate()
+        print("Miner stopped successfully!")
+
+def proofOfWork(event, block, publicAddress):#Core mining module, does the work when mining
+    start = time.time()
+    found = False
+    challenge = block["hash"] + "80"
+    while found == False:
+        answer = "".join(random.choice(string.ascii_lowercase+
+                           string.ascii_lowercase+
+                           string.digits) for x in range(80))
+        attempt = challenge+answer
+        solution = hashlib.sha256(attempt.encode()).hexdigest()
+        if solution.startswith('000000'):#DONT change this either, will be rejected by server - should be 000000
+            found = True
+            print("\nBLOCK MINED! Time taken:", time.time()-start)
+            print("Timestamp:",time.time())
+            print(solution)
+            start = time.time()
+            block["attempt"] = attempt
+            block["miner"] = publicAddress
+            time.sleep(1)
+            res = requests.post("http://35.209.72.253:443/confirmBlock", data=block)
+            event.set()
+    return True
+    
+def minerButton(vs,quitEvent):#run when the mining button is pressed, best not to edit this
+    if vs.quit == True:
+        vs.quit = False
+        quitEvent.clear()
         btnMine_text.set("STOP MINING")
         print("""\nWARNING!
 MoagCoin mining is CPU intensive.
 Depending on device performance, mining MoagCoin may take time.
 If you stop mining, you will lose the block that's currently being mined.""")
-        minerThread = threading.Thread(target=Miner.miner).start()#Starts miner thread seperate to rest of UI thread
+        #minerThread = threading.Thread(target=Miner.miner).start()#Starts miner thread seperate to rest of UI thread
+        minerProc = mp.Process(target=miner,args=[vs,quitEvent,]).start()
+
     else:
-        print("MINING STOPPING...")
-        minerClass.quit = False
+        print("MINING STOPPING, PLEASE WAIT A FEW SECONDS FOR PROCESSES TO END!")
+        vs.quit = True
+        quitEvent.set()
         btnMine_text.set("START MINING")
 
-#this is pretty much all UI stuff, mostly safe to edit. do not touch bottom 2 lines or UI will break
-label = Label(root,background="black",text="MOAGCOIN INTERFACE v1",foreground="green",font=("Courier New", 24))
-label.pack()
-label = Label(root,background="black")
-label.pack()
-lblMotd = Label(root,background="black",text=motd(),foreground="green",font=("Courier New", 18))
-lblMotd.pack()
-padding = Label(root,background="black",text=" ",font=("Courier New", 18))
-padding.pack()
-btnWallet = Button(root,background="black",text="OPEN WALLET",foreground="green",font=("Courier New", 24),command=wallet)
-btnWallet.pack()
-btnMine_text = StringVar()
-btnMine_text.set("START MINING")
-btnMine = Button(root,background="black",textvariable=btnMine_text,foreground="green",font=("Courier New", 24), command=lambda: minerButton(Miner))
-btnMine.pack()
-btnMusic = Button(root,background="black",text="STOP MUSIC",foreground="green",font=("Courier New", 16), command=lambda: winsound.PlaySound(None, winsound.SND_PURGE))
-btnMusic.pack()
+if __name__ == '__main__':
+    mp.freeze_support()#Important for compilation with PyInstaller, won't work properly without it
+    print("""
+ ███▄ ▄███▓ ▒█████   ▄▄▄        ▄████  ▄████▄   ▒█████   ██▓ ███▄    █ 
+▓██▒▀█▀ ██▒▒██▒  ██▒▒████▄     ██▒ ▀█▒▒██▀ ▀█  ▒██▒  ██▒▓██▒ ██ ▀█   █ 
+▓██    ▓██░▒██░  ██▒▒██  ▀█▄  ▒██░▄▄▄░▒▓█    ▄ ▒██░  ██▒▒██▒▓██  ▀█ ██▒
+▒██    ▒██ ▒██   ██░░██▄▄▄▄██ ░▓█  ██▓▒▓▓▄ ▄██▒▒██   ██░░██░▓██▒  ▐▌██▒
+▒██▒   ░██▒░ ████▓▒░ ▓█   ▓██▒░▒▓███▀▒▒ ▓███▀ ░░ ████▓▒░░██░▒██░   ▓██░
+░ ▒░   ░  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ░▒   ▒ ░ ░▒ ▒  ░░ ▒░▒░▒░ ░▓  ░ ▒░   ▒ ▒ 
+░  ░      ░  ░ ▒ ▒░   ▒   ▒▒ ░  ░   ░   ░  ▒     ░ ▒ ▒░  ▒ ░░ ░░   ░ ▒░
+░      ░   ░ ░ ░ ▒    ░   ▒   ░ ░   ░ ░        ░ ░ ░ ▒   ▒ ░   ░   ░ ░ 
+       ░       ░ ░        ░  ░      ░ ░ ░          ░ ░   ░           ░ 
+                                      ░                                
+                    --===============================--
+                          MOAGCOIN MINER & WALLET
+                    --===============================--
+""")#highly important performance enhancing ascii art
 
-
-root.after(0, update, 0)
-root.mainloop()
+    root = Tk()
+    root.configure(background='black')
+    root.title("MoagCoin")
+    vs = varSave()
+    quitEvent = mp.Event()
+    try:
+        root.iconbitmap('res\\icon.ico')
+    except:
+        try:
+            root.iconbitmap('res/icon.ico')#fuckin linux users, man. will this work? idk
+        except:
+            pass
+    frames = [PhotoImage(file='res\\MoagCoin.gif',format = 'gif -index %i' %(i)) for i in range(79)]
+    #this is pretty much all UI stuff, mostly safe to edit. do not touch bottom 2 lines or UI will break
+    label = Label(root,background="black",text="MOAGCOIN INTERFACE v2",foreground="green",font=("Courier New", 24))
+    label.pack()
+    label = Label(root,background="black")
+    label.pack()
+    lblMotd = Label(root,background="black",text=motd(),foreground="green",font=("Courier New", 18))
+    lblMotd.pack()
+    padding = Label(root,background="black",text=" ",font=("Courier New", 18))
+    padding.pack()
+    btnWallet = Button(root,background="black",text="OPEN WALLET",foreground="green",font=("Courier New", 24),command=wallet)
+    btnWallet.pack()
+    btnMine_text = StringVar()
+    btnMine_text.set("START MINING")
+    btnMine = Button(root,background="black",textvariable=btnMine_text,foreground="green",font=("Courier New", 24), command=lambda: minerButton(vs,quitEvent))
+    btnMine.pack()
+    root.after(0, update, 0)
+    root.mainloop()
